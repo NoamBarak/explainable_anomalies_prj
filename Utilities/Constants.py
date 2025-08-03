77ab36d11e6cf37adbb187805a7bf622a5e44795
Noam Barak <57773856+NoamBarak@users.noreply.github.com>
2025-08-03 18:13:25 +0300

    Add files via upload

---
+++ Utilities/Constants.py
@@ -0,0 +1,19 @@
+PROJECT_PATH = r'C:\Users\User\PycharmProjects\MSc\Deep_Generative_Models\XAD_FinalProject'
+
+MIN_ROWS_AMOUNT = 3
+MAX_ROWS_AMOUNT = 3
+
+MIN_COLS_AMOUNT = 3
+MAX_COLS_AMOUNT = 5
+
+SUBSETS_AMOUNT = 3
+
+OMEGA_1 = 0.7
+OMEGA_2 = 0.15
+OMEGA_3 = 0.15
+
+# COLUMNS = ['OverallQual', 'GarageArea', 'GrLivArea', 'GarageCars', 'YearBuilt', 'WoodDeckSF', 'LotArea',
+#            'TotalBsmtSF', 'GarageYrBlt']
+
+COLUMNS = ['Overall Qual', 'Gr Liv Area', '1st Flr SF', 'Full Bath', '2nd Flr SF', 'Garage Cars', 'Total Bsmt SF',
+           'Lot Area']
